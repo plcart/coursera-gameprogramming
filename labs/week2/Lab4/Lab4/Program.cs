187009af02f960ac392d316f0160dcbe4c42fbfd
Arthur Ferreira <plc_4art@hotmail.com>
2015-03-01 11:47:00 -0300

    lab week 2 content

---
+++ labs/week2/Lab4/Lab4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab4
+{
+	/// <summary>
+	/// Demonstrates classes and objects
+	/// </summary>
+    class Program
+    {
+		/// <summary>
+		/// Demonstrates use of Deck and Card objects
+		/// </summary>
+		/// <param name="args">command-line args</param>
+        static void Main(string[] args)
+        {
+            Deck objDesck = new Deck();
+            objDesck.Print();
+
+            objDesck.Shuffle();
+            objDesck.Print();
+
+            Card TopCard = objDesck.TakeTopCard();
+
+            Console.WriteLine("{0} of {1}", TopCard.Rank, TopCard.Suit);
+            Console.Read();
+
+        }
+    }
+}
